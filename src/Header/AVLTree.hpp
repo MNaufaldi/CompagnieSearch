9b96f11e0fdd51471fdc4d6cd0c5b4cff8a8af69
IlzJokerzlI <arvin.lee@binus.ac.id>
2020-06-03 22:08:51 +0700

    Added comments to AVLTree classes

--- src/Header/AVLTree.hpp
+++ src/Header/AVLTree.hpp
@@ -4,15 +4,15 @@
 class AVLTree {
 	Node *root {nullptr};
 
-	int getHeight(Node *node);
-	int getBalanceFactor(Node *node);
-	Node *rotateToR(Node *node);
-	Node *rotateToLToR(Node *node);
-	Node *rotateToL(Node *node);
-	Node *rotateToRToL(Node *node);
-	Node *insert(Node *node, int value);
+	int getHeight(Node *node); //Get the height position of nodes in the tree
+	int getBalanceFactor(Node *node); //Get balance factor for nodes to determine their rotation
+	Node *rotateToR(Node *node); //Rotation for LL Imbalance
+	Node *rotateToLToR(Node *node); //Rotation for LR Imbalance
+	Node *rotateToL(Node *node); //Rotation for RR Imbalance
+	Node *rotateToRToL(Node *node); //Rotation for RL Imbalance
+	Node *insert(Node *node, int value); //Insert node
 
 	public:
-		Node *getRoot();
-		int insert(int value);
+		Node *getRoot(); //Get the root node of the tree
+		int insert(int value); //Insert node for public uses
 };
--- src/Implementation/AVLTree.cpp
+++ src/Implementation/AVLTree.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 
 
-//Get the height of a node
+//Gets the height of a node
 int AVLTree::getHeight(Node *node) {
     int lHeight = (node && node->lPtr) ? node->lPtr->height : 0;
     int rHeight = (node && node->rPtr) ? node->rPtr->height : 0;
@@ -21,15 +21,18 @@ int AVLTree::getBalanceFactor(Node *node) {
 }
 
 
-//Perform node rotation To Right (LL Imbalance)
+//Performs node rotation To Right (LL Imbalance)
 Node *AVLTree::rotateToR(Node *node) {
+    //Sets the position of nodes
     Node *temp = node->lPtr;
     node->lPtr = temp->rPtr;
     temp->rPtr = node;
 
+    //Updates the height of swaped nodes
     node->height = this->getHeight(node);
     temp->height = this->getHeight(temp);
 
+    //Updates the root if the position of previous root swapped
     if (node == this->root) {
         this->root = temp;
     }
@@ -38,18 +41,21 @@ Node *AVLTree::rotateToR(Node *node) {
 }
 
 
-//Perform node rotation To Left then To Right (LR Imbalance)
+//Performs node rotation To Left then To Right (LR Imbalance)
 Node *AVLTree::rotateToLToR(Node *node) {
+    //Sets the position of nodes
     Node *temp = node->lPtr->rPtr;
     node->lPtr->rPtr = temp->lPtr;
     temp->lPtr = node->lPtr;
     node->lPtr = temp->rPtr;
     temp->rPtr = node;
 
+    //Updates the height of swaped nodes
     node->height = this->getHeight(node);
     temp->height = this->getHeight(node);
     temp->lPtr->height = this->getHeight(temp->lPtr);
 
+    //Updates the root if the position of previous root swapped
     if (node == this->root) {
         this->root = temp;
     }
@@ -61,13 +67,16 @@ Node *AVLTree::rotateToLToR(Node *node) {
 
 //Perform node rotation To Left (RR Imbalance)
 Node *AVLTree::rotateToL(Node *node) {
+    //Sets the position of nodes
     Node *temp = node->rPtr;
     node->rPtr = temp->rPtr;
     temp->lPtr = node;
 
+    //Updates the height of swaped nodes
     node->height = this->getHeight(node);
     temp->height = this->getHeight(temp);
 
+    //Updates the root if the position of previous root swapped
     if (node == this->root) {
         this->root = temp;
     }
@@ -76,18 +85,21 @@ Node *AVLTree::rotateToL(Node *node) {
 }
 
 
-//Perform node rotation To Right then To Left (RL Imbalance)
+//Performs node rotation To Right then To Left (RL Imbalance)
 Node *AVLTree::rotateToRToL(Node *node) {
+    //Sets the position of nodes
     Node *temp = node->rPtr->lPtr;
     node->rPtr->lPtr = temp->rPtr;
     temp->rPtr = node->rPtr;
     node->rPtr = temp->lPtr;
     temp->lPtr = node;
 
+    //Updates the height of swaped nodes
     node->height = this->getHeight(node);
     temp->height = this->getHeight(node);
     temp->rPtr->height = this->getHeight(temp->rPtr);
 
+    //Updates the root if the position of previous root swapped
     if (node == this->root) {
         this->root = temp;
     }
@@ -96,23 +108,29 @@ Node *AVLTree::rotateToRToL(Node *node) {
 }
 
 
-//Inserting value
+//Inserts value to tree
 Node *AVLTree::insert(Node *node, int value) {
+    //Set the root if none presents in the tree
     if (this->root == nullptr) {
         this->root = new Node(value);
         return this->root;
     }
 
+    //Create new node at the leaf of the tree
     if (node == nullptr) {
         node = new Node(value);
         return node;
     }
 
+    //Get to the leaf of the tree
     if (value < node->value) {
+        //Put left if value is smaller
         node->lPtr = insert(node->lPtr, value);
     } else if (value > node->value) {
+        //Put right if value is bigger
         node->rPtr = insert(node->rPtr, value);
     } else {
+        //Put to linked list if value is equal
         Node *tempNode = node
         while(tempNode->cPtr) {
             tempNode = tempNode->cPtr;
@@ -120,15 +138,21 @@ Node *AVLTree::insert(Node *node, int value) {
         tempNode->cPtr = new Node(value);
     }
 
+    //Update the height of the nodes traversed by the new value
     node->height = this->getHeight(node);
 
+    //Check which rotation to take
     if (this->getBalanceFactor(node) == 2 && this->getBalanceFactor(node->lPtr) == 1) {
+        //LL Imbalance
         return this->rotateToR(node);
     } else if (this->getBalanceFactor(node) == 2 && this->getBalanceFactor(node->lPtr) == -1) {
+        //LR Imbalance
         return this->rotateToLToR(node);
     } else if (this->getBalanceFactor(node) == -2 && this->getBalanceFactor(node->rPtr) == -1) {
+        //RR Imbalance
         return this->rotateToL(node);
     } else if (this->getBalanceFactor(node) == -2 && this->getBalanceFactor(node->rPtr) == 1) {
+        //RL Imbalance
         return this->rotateToRToL(node);
     }
 
@@ -136,8 +160,11 @@ Node *AVLTree::insert(Node *node, int value) {
 }
 
 
-//Public Function
+//Public Functions
+
+//Inserts value to tree
 int AVLTree::insert(int value) {
+    //Try-Catch error from insert function
     try {
         this->insert(this->getRoot(), value);
     } catch (...) {
@@ -148,7 +175,7 @@ int AVLTree::insert(int value) {
 }
 
 
-//Get the root of the tree
+//Gets the root of the tree
 Node *AVLTree::getRoot() {
     return this->root;
 }
